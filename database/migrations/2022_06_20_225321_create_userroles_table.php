941d332a4c221b278102684bfa29ca40e26c2795
minmi <minmi@MSI>
2022-06-27 01:56:47 +0300

    added laravel breeze (new project, copied files)

---
+++ database/migrations/2022_06_20_225321_create_userroles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userroles', function (Blueprint $table) {
+            $table->id();
+            $table->string('rolename');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('userroles');
+    }
+};
